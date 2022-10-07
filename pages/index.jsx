97fc56db6c82a9316b77a306b0df119e44e76cbb
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-10-07 10:01:42 -0500

    Upggrade index formato

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+//Importaciones
+import { MainLayout } from "../components/Layouts/MainLayout";
+import { TextBlock } from "../components/TextBlock";
+
+const Dependencias = () => {
+  return (
+    <>
+      <MainLayout>
+        <TextBlock />
+      </MainLayout>
+    </>
+  );
+};
+
+export default Dependencias;
