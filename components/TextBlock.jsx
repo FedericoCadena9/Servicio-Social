98fc079a6e7fffcacefce5843fe9b0b71a8b66a2
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-10-07 10:31:56 -0500

    Update Componente TextBlock

--- components/TextBlock.jsx
+++ components/TextBlock.jsx
@@ -1,25 +1,14 @@
-import Link from 'next/link'
 
-//Iconos
-import { Squares2X2Icon } from '@heroicons/react/24/outline'
-// Icon Plus
-import { PlusIcon } from '@heroicons/react/24/outline'  
-
-export const TextBlock = () => {
-  return (
-    <div className='px-8 py-4 flex items-center justify-between'>
-        <div>
-            <h1 className='text-gray-900 font-bold text-4xl font-secondary'>Dependencias</h1>
-            <p className='text-gray-500 mt-2'>123 resultados encontrados </p>
+export const TextBlock = ({ title, subtitle, children }) => {
+    return (
+        <div className='px-8 py-4 flex items-center justify-between'>
+            <div>
+                <h1 className='text-gray-900 font-bold text-4xl font-secondary'>{title}</h1>
+                <p className='text-gray-500 mt-2'>{subtitle}</p>
+            </div>
+            <div className="">
+                {children}
+            </div>
         </div>
-        <div className="">
-            <Link href={'/'}>
-                <a className='btn btn-primary flex items-center gap-2'>
-
-                    <span>Agregar Dependencia</span>
-                </a>
-            </Link>
-        </div>
-    </div>
-  )
+    )
 }
