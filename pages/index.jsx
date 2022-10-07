09535c92e1e0e74c3631cb8a865b788f5a3cabd6
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-10-07 10:32:08 -0500

    Add TextBlock Componente

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,19 @@
 import { MainLayout } from "../components/Layouts/MainLayout";
 import { TextBlock } from "../components/TextBlock";
 
+
+// Iconos
+import { PlusIcon } from "@heroicons/react/20/solid";
 const Dependencias = () => {
   return (
     <>
       <MainLayout>
-        <TextBlock />
+        <TextBlock title={'Dependencias'} subtitle={'123 resultados encontrados'}>
+          <button className='btn btn-primary'>
+            <PlusIcon className='w-5 h-5 mr-2' />
+            <span>Agregar Dependencia</span>
+          </button>
+        </TextBlock>
       </MainLayout>
     </>
   );
