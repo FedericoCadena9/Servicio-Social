9cfd968fa0611dc5943ddb5b6a12df0c53bdf231
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-10-07 10:01:26 -0500

    Create documentos.jsx

---
+++ pages/documentos.jsx
@@ -0,0 +1,13 @@
+import { MainLayout } from "../components/Layouts/MainLayout";
+
+const Documentos = () => {
+  return (
+    <>
+      <MainLayout>
+        <p>Documentos</p>
+      </MainLayout>
+    </>
+  );
+};
+
+export default Documentos;
