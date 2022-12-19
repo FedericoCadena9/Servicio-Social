d5933a27a33958d63364887395932abe6ce303d6
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-12-18 22:05:12 -0600

    Update CorrectoAsignacionData.jsx

--- components/alumnos/CorrectoAsignacionData.jsx
+++ components/alumnos/CorrectoAsignacionData.jsx
@@ -22,7 +22,7 @@ const CorrectoAsignacionData = ({ data }) => {
 
     const view = new Uint8Array(buf);
 
-    for (let i = 0; i !== s.length; ++i) view[i] = s.charCodeAt(i);
+    for (let i = 0; i !== s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
 
     return buf;
   }
